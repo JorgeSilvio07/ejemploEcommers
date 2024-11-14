36a1cda60df498a124d98bf8910d6ce1df1959db
Jorge <boyka604jr@gmail.com>
2024-11-13 23:05:24 -0600

    creando mi repositorio local

---
+++ src/test/java/com/commers/CommersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
